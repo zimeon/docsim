43ea70e31508eb1af0fae482420414a57aaebf58
Simeon Warner <simeon.warner@cornell.edu>
2014-08-15 10:09:38 +0200

    Tidy test_pstats

--- cpp/lib/test_pstats.cpp
+++ cpp/lib/test_pstats.cpp
@@ -2,19 +2,11 @@
 #include <stdio.h>
 #include "pstats.h"
 
-char str[80];
-
 int main() {
-  printf("The printf way:\n\n");
+  printf("Getting pstats 10 times with 1s delay:\n");
   for (int j=1; j<11; j++) {
-    get_pstats_string(str,80);
-    printf("get_pstats_string[%02d]: %s\n",j,str);
-  }
-
-  cout << endl << endl << "The cout way:" << endl << endl;
-  for (int j=1; j<11; j++) {
-    cout << "write_pstats_string[" << j << "]: ";
-    write_pstats_string(cout);
-    cout << endl;
+    string pstats=get_pstats_string();
+    printf("get_pstats_string[%02d]: %s\n",j,pstats.c_str());
+    sleep(1);
   }
 }
